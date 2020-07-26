2bbbf28a03abdf9dce01a2a0f5c1c15fa65523f0
Carrigan <Carriganreilly@gmail.com>
2020-07-26 00:11:56 -0400

    Update homepagejs.js

--- homepagejs.js
+++ homepagejs.js
@@ -1,1 +1,7 @@
 alert('ok');
+window.onload=newIntro;
+function newIntro() {
+  var intros = ['Fact fsdsfd', 'Fact 2', 'Fact 183'];
+  var randomIntro = Math.floor(Math.random() * intros.length);
+  document.getElementById('introDisplay').innerHTML = intros[randomIntro];
+}
