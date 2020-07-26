a7f39610118b2f86924d325314cae28dad627cca
Carrigan <Carriganreilly@gmail.com>
2020-07-26 00:11:04 -0400

    only alert

--- homepagejs.js
+++ homepagejs.js
@@ -1,7 +1,1 @@
-(function newIntro() {
-  var intros = ['Fact fsdsfd', 'Fact 2', 'Fact 183'];
-  var randomIntro = Math.floor(Math.random() * intros.length);
-  document.getElementById('introDisplay').innerHTML = intros[randomIntro];
-})(); 
-window.onload = newIntro;
 alert('ok');
