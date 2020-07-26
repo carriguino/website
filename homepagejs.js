2c0e051fffc4755076288aae56708834670e0660
Carrigan <Carriganreilly@gmail.com>
2020-07-26 15:45:38 -0400

    more headers
    +secret message in code

--- homepagejs.js
+++ homepagejs.js
@@ -1,6 +1,11 @@
 window.onload=newIntro;
 function newIntro() {
-  var intros = ['Welcome!', 'go back just trust me', 'Minceraft', 'Carri\'s Domain', 'Carri\'s Artsite!', 'hi u///u', '\'sup.', 'Hey! You\'re finally awake!', 'Welcome to my site!', 'oh, hey i guess...', 'Thank\'s for coming!'];
+  var intros = ['Welcome!', 'go back just trust me', 'Minceraft', 'Carri\'s Domain',
+   'Carri\'s Artsite!', 'hi u///u', '\'sup.', 'Hey! You\'re finally awake!', 'Welcome to my site!',
+   'oh, hey i guess...', 'Thank\'s for comin\'g!', '👁️  👁️', 'Have a nice stay!', 'Welcome to tha site :O', 'Good website choice, I like your style.',
+   'Humans only', 'Robots only', 'Catgirls only', 'LGBTQ+ safespace', 'A homophobe\'s unsafespace.', 'teehee', '*various whispers*', 'Like what you see?',
+   'Gamers only', 'lol', 'Thanks for coming!', 'Welcome friend :)', 'Heya, welcome', 'Not Spyware', 'Don\'t look at my code, baka! O//O'];
+   
   var randomIntro = Math.floor(Math.random() * intros.length);
   document.getElementById('introDisplay').innerHTML = intros[randomIntro];
 }
