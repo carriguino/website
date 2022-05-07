d0bad89607d939826d6bd6544a6d1a354b7f7c9c
Carrigan <Carriganreilly@gmail.com>
2022-05-07 12:19:21 -0400

    css and js cleanup

--- portfolio/modal.js
+++ portfolio/modal.js
@@ -1,19 +1,19 @@
 // modal code adapted from w3Schools and jquery code from user ab29007 on StackOverflow
-                   
+
 var modal = document.getElementById('myModal');
 
 var img = $('.myImg');
 var modalImg = $("#img01");
 var captionText = document.getElementById("caption");
-$('.myImg').click(function(){
-    modal.style.display = "block";
-    var newSrc = this.src;
-    modalImg.attr('src', newSrc);
-    captionText.innerHTML = this.alt;
+$('.myImg').click(function () {
+  modal.style.display = "block";
+  var newSrc = this.src;
+  modalImg.attr('src', newSrc);
+  captionText.innerHTML = this.alt;
 });
 
 var span = document.getElementsByClassName("modal")[0];
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
