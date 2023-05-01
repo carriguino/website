4b8d0faa8fcba2a2ded39ff7b1e9e1faaec0423d
Carrigan <Carriganreilly@gmail.com>
2023-05-01 11:55:56 -0400

    JSON BABY

--- portfolios/modal.js
+++ portfolios/modal.js
@@ -1,16 +1,17 @@
 // modal code adapted from w3Schools and jquery code from user ab29007 on StackOverflow
-
 var modal = document.getElementById('myModal');
+window.onload = function () { //loads modal after the page is loaded so that getJSON.js is run first and the click event attaches to created elements
+  var img = $('.myImg');
+  var modalImg = $("#img01");
+  var captionText = document.getElementById("caption");
+  $('.myImg').click(function () {
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+  });
+}
 
-var img = $('.myImg');
-var modalImg = $("#img01");
-var captionText = document.getElementById("caption");
-$('.myImg').click(function () {
-  modal.style.display = "block";
-  var newSrc = this.src;
-  modalImg.attr('src', newSrc);
-  captionText.innerHTML = this.alt;
-});
 
 var span = document.getElementsByClassName("modal")[0];
 
